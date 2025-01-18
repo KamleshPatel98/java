12a12555bb963d8752839cae08817eed2934233a
KamleshPatel98 <kamleshp52170@gmail.com>
2025-01-19 00:29:31 +0530

    arithmetic

---
+++ Arithmetic.java
@@ -0,0 +1,11 @@
+class Arithmetic {
+    public static void main(String[] arg){
+        int a = 24;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(sum); 
+
+        int quiz = (a * b) / (a - b);
+        System.out.println(quiz);
+    }
+}
