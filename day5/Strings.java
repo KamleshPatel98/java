2bf7e67bb0c3df3969f385a21fdc83f107c6c32d
KamleshPatel98 <kamleshp52170@gmail.com>
2025-01-24 22:37:22 +0530

    string

---
+++ day5/Strings.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+
+        // string are immutable 
+
+        // System.out.print("Enter string");
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // String surname = sc.nextLine();
+        // System.out.println(surname);
+
+        // Concatation
+        // System.out.println(name + " " + surname);
+
+        // length
+        // System.out.println(name.length());
+
+        // character print
+        // for(int i=0; i<name.length(); i++){
+        //     System.out.println(name.charAt(i));
+        // }
+
+
+        // compare
+        // String name1 = sc.nextLine();
+        // String name2 = sc.nextLine();
+        // s1 > s2 => +ve (value)
+        // s1 == s2 => 0 (equal value)
+        // s1 < s2 => -ve  (value)
+
+        // if(name1.compareTo(name2) == 0){
+        //     System.out.println("Name are equal!");
+        // }else{
+        //     System.out.print("Name are not equal");
+        // }
+
+
+        // substring
+        // String sentence = "kamleshPatel";
+        // String name = sentence.substring(1,4);
+        // System.out.println(name);
+        // String name = sentence.substring(4); //ending point
+        // System.out.println(name);
+    }
+}
